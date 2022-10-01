c4327a3e85f5ec6d5cd5457d67149de6d1588e8f
maiko-m <mn.6182.nm@gmail.com>
2022-10-01 23:41:39 +0900

    init

---
+++ renovate-sample/renovate_sampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  renovate_sampleApp.swift
+//  renovate-sample
+//
+//  Created by maiko morisaki on 2022/10/01.
+//
+
+import SwiftUI
+
+@main
+struct renovate_sampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
